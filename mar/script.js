b7f97f2c4bcdbe2542a20874286ec22c3a241363
mari266 <119744590+mari266@users.noreply.github.com>
2022-12-17 12:21:00 +0300

    Add files via upload

---
+++ mar/script.js
@@ -0,0 +1,547 @@
+
+
+
+
+
+
+let div = document.createElement("div")
+div.style.width = "100%"
+div.style.height = "70px"
+div.style.backgroundColor = "#20B2AA"
+document.body.append(div)
+let kart=document.createElement("img")
+kart.style.backgroundImage="url(./img/icons8-вода-100.png)"
+kart.style.backgroundSize="cover"
+kart.style.width="70px"
+kart.style.height="70px"
+kart.setAttribute("align","left")
+div.append(kart)
+let tex=document.createElement("p")
+tex.innerHTML="<h1>Аквопарк Водичка</h1>"
+tex.style.textAlign="center"
+div.append(tex)
+
+document.body.append(div)
+// div.innerHTML="<h1>ill</h1>"
+
+
+
+// document.body.style.backgroundImage
+
+
+
+let iv = document.createElement("div")
+iv.style.width = "100%"
+iv.style.height = "2000px"
+iv.style.marginTop = "10px"
+iv.style.backgroundColor = "#5F9EA0"
+document.body.append(iv)
+
+
+
+
+let obs= document.createElement("div")
+obs.style.width = "20%"
+obs.style.height = "2000px"
+obs.style.marginTop = "10px"
+// one.style.display="flex"
+obs.style.backgroundColor = " #008080"
+document.body.append(obs)
+iv.append(obs)
+
+
+let one = document.createElement("div")
+one.style.width = "20%"
+one.style.height = "2000px"
+one.style.marginTop = "10px"
+// one.style.display="flex"
+one.style.backgroundColor = " #008080"
+document.body.append(one)
+iv.append(one)
+obs.append(one)
+
+
+let oo = document.createElement("div")
+oo.style.width = "100%"
+oo.style.height = "50px"
+oo.style.marginTop = "30px"
+oo.innerHTML="<h1>-Про нас</h1>"
+oo.style.backgroundColor = "#008080"
+document.body.append(oo)
+one.append(oo)
+
+let ot= document.createElement("div")
+ot.style.width = "100%"
+ot.style.height = "50px"
+ot.style.marginTop = "60px"
+ot.innerHTML="<h1>-Цены</h1>"
+ot.style.backgroundColor = "#008080 "
+document.body.append(ot)
+one.append(ot)
+
+let oth = document.createElement("div")
+oth.style.width = "100%"
+oth.style.height = "50px"
+oth.style.marginTop = "60px"
+oth.innerHTML="<h1>-Горки</h1>"
+oth.style.backgroundColor = " #008080"
+document.body.append(oth)
+one.append(oth)
+
+let of = document.createElement("div")
+of.style.width = "100%"
+of.style.height = "50px"
+of.style.marginTop = "50px"
+of.innerHTML="<h1>-Прайс</h1> "
+of.style.backgroundColor = " #008080"
+document.body.append(of)
+one.append(of)
+
+let oi = document.createElement("div")
+oi.style.width = "100%"
+oi.style.height = "50px"
+oi.style.marginTop = "50px"
+oi.innerHTML="<h1>-Где нас найти</h1> "
+oi.style.backgroundColor = " #008080"
+document.body.append(oi)
+one.append(oi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let two = document.createElement("div")
+two.style.width = "60%"
+two.style.height = "2000px"
+two.style.marginTop = "10px"
+two.style.backgroundColor = " #20B2AB"
+document.body.append(two)
+iv.append(two)
+// obs.append(two)
+
+
+let vt = document.createElement("div")
+vt.style.width = "100%"
+vt.style.height = "20px"
+vt.style.marginTop = "5px"
+let textpro=document.createElement("p")
+textpro.innerHTML="<h1>Про нас</h1>"
+
+
+
+textpro.style.textAlign="center"
+two.append(textpro)
+document.body.append(vt)
+two.append(vt)
+
+let kar=document.createElement("img")
+kar.style.backgroundImage="url(./img/60-11.jpg)"
+kar.style.backgroundSize="cover"
+kar.style.width="100%"
+kar.style.height="400px"
+two.append(kar)
+
+
+
+let fbo = document.createElement("div")
+fbo.style.width = "100%"
+fbo.style.height = "200px"
+fbo.style.marginTop = "30px"
+fbo.style.display="flex"
+fbo.innerHTML="<br>С каким бы нетерпением мы ни ждали долгожданного лета, чтобы поехать к морю, на определенный день тюленьего пляжного отдыха многие из нас начинают скучать и ищут более активных развлечений. Посещение аквапарка может стать отличным вариантом — это весело, свежо и чаще всего доступно. В Краснодарском крае аквапарков масса, в основном они сосредоточены в курортных городах Азовского и Черноморского побережий."
+document.body.append(fbo)
+two.append(fbo)
+
+
+
+
+
+
+let th = document.createElement("div")
+th.style.width = "100%"
+th.style.height = "500px"
+th.style.marginTop = "10px"
+let pr=document.createElement("img")
+pr.style.backgroundImage="url(./img/прайс.jpg)"
+pr.style.backgroundSize="cover"
+pr.style.width="100%"
+pr.style.height="500px"
+th.append(pr)
+
+th.style.backgroundColor = " #296880"
+document.body.append(th)
+iv.append(th)
+obs.append(th)
+two.append(th)
+
+
+
+let ss = document.createElement("div")
+ss.style.width = "100%"
+ss.style.height = "350px"
+ss.style.marginTop = "35px"
+ss.innerHTML="<br><h1>Типы горок:</h1><br><br><br>Плоская сжатая арка<br><br><br>Круглая или полуциркульная<br><br><br>Трёхлопастная арка."
+ss.style.backgroundColor = " #296880"
+document.body.append(ss)
+iv.append(ss)
+obs.append(ss)
+two.append(ss)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let ree = document.createElement("div")
+ree.style.width = "20%"
+ree.style.height = "660px"
+ree.style.marginTop = "15px"
+let k=document.createElement("img")
+k.style.backgroundImage="url(./img/2.jpg)"
+k.style.backgroundSize="cover"
+k.style.width="100%"
+k.style.height="330px"
+ree.append(k)
+
+
+let kq=document.createElement("img")
+kq.style.backgroundImage="url(./img/1.jpg)"
+kq.style.backgroundSize="cover"
+kq.style.width="100%"
+kq.style.height="325px"
+ree.append(kq)
+ree.style.backgroundColor = "#BDB76B"
+document.body.append(ree)
+iv.append(ree)
+
+
+
+
+
+
+
+
+
+
+
+
+let m = document.createElement("div")
+m.style.width = "20%"
+m.style.height = "660px"
+m.style.marginTop = "10px"
+m.style.backgroundColor = "#5F9EA0"
+
+function changeBodyColor(blue){
+   document.body.style.background="yellow"
+}
+document.body.onclick=function(){
+
+
+changeBodyColor("red")
+}
+let qk=document.createElement("img")
+ qk.style.backgroundImage="url(./img/01.jpg)"
+ qk.style.backgroundSize="cover"
+ qk.style.marginTop = "50px"
+ qk.style.width="100%"
+ qk.style.height="90px"
+ m.append(qk)
+
+
+ let n=document.createElement("img")
+ n.style.backgroundImage="url(./img/01.jpg)"
+ n.style.backgroundSize="cover"
+ n.style.marginTop = "50px"
+ n.style.width="100%"
+ n.style.height="90px"
+ m.append(n)
+
+
+
+let uu=document.createElement("img")
+uu.style.backgroundImage="url(./img/01.jpg)"
+uu.style.backgroundSize="cover"
+uu.style.width="100%"
+uu.style.marginTop = "50px"
+uu.style.height=" 90px"
+m.append(uu)
+
+
+let up=document.createElement("img")
+up.style.backgroundImage="url(./img/01.jpg)"
+up.style.backgroundSize="cover"
+up.style.marginTop = "50px"
+up.style.width="100%"
+up.style.height=" 90px"
+m.append(up)
+
+document.body.append(m)
+iv.append(m)
+
+
+
+
+
+
+
+
+
+
+
+let l = document.createElement("div")
+l.style.width = "20%"
+l.style.height = "660px"
+function changeBodyColor(blue){
+   document.body.style.background="green"
+}
+document.body.onclick=function(){
+
+
+changeBodyColor("red")
+}
+l.style.marginTop = "15px"
+let lk=document.createElement("img")
+lk.style.backgroundImage="url(./img/37.jpg)"
+lk.style.backgroundSize="cover"
+lk.style.width="100%"
+lk.style.height="360px"
+l.append(lk)
+let aa=document.createElement("img")
+aa.style.backgroundImage="url(./img/37.jpg)"
+aa.style.backgroundSize="cover"
+aa.style.width="100%"
+aa.style.height="360px"
+l.append(aa)
+l.style.backgroundColor = "#BDB76B"
+document.body.append(l)
+iv.append(l)
+
+
+
+
+
+
+
+// let l = document.createElement("div")
+// l.style.width = "20%"
+// l.style.height = "660px"
+// l.style.marginTop = "10px"
+// l.style.backgroundColor = "#5F9EA0"
+
+// let lh=document.createElement("img")
+// lh.style.backgroundImage="url(./img/.7jpg)"
+// lh.style.backgroundSize="cover"
+// lh.style.width="100%"
+// lh.style.height="600px"
+//  l.append(lh)
+
+//  document.body.append(l)
+//  iv.append(l)
+
+
+
+
+
+
+// let uu=document.createElement("img")
+// uu.style.backgroundImage="url(./img/5.png)"
+// uu.style.backgroundSize="cover"
+// uu.style.width="100%"
+// uu.style.height=" 220px"
+// ee.append(uu)
+
+
+// let up=document.createElement("img")
+// up.style.backgroundImage="url(./img/t.png)"
+// up.style.backgroundSize="cover"
+// up.style.width="100%"
+// up.style.height=" 90px"
+// ee.append(up)
+
+
+
+
+
+
+// let cc= document.createElement("div")
+// cc.style.width = "100%"
+// cc.style.height = "500px"
+// cc.style.marginTop = "10px"
+// cc.style.display="flex"
+// cc.style.backgroundColor = " #3893000"
+// document.body.append(cc)
+// two.append(cc)
+
+// let con = document.createElement("div")
+// con.style.width = "100%"
+// con.style.height = "500px"
+// con.style.marginTop = "10px"
+// con.style.display="flex"
+// let ktt=document.createElement("img")
+// ktt.style.backgroundImage="url(./img/еда.jpg)"
+// ktt.style.backgroundSize="cover"
+// ktt.style.width="1000px"
+// ktt.style.height="450px"
+// con.append(ktt)
+// tbovt.style.backgroundColor = "#B0E0E6 "
+// document.body.append(con)
+
+// cc.append(con)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let hh = document.createElement("div")
+// hh.style.width = "20%"
+// hh.style.height = "666px"
+// hh.style.marginTop = "10px"
+// hh.innerHTML="нпгршоргнапгршощ"
+// hh.style.backgroundColor = " #FFA07A"
+// document.body.append(hh)
+// iv.append(hh)
+// hh.append(bok)
+
+
+
+
+// let ree = document.createElement("div")
+// ree.style.width = "20%"
+// ree.style.height = "666px"
+// ree.style.marginTop = "10px"
+// ree.innerHTML="ooooooo"
+// ree.style.backgroundColor = "#FFA07A"
+// document.body.append(ree)
+// iv.append(ree)
+
+
+
+// let ee = document.createElement("div")
+// ee.style.width = "20%"
+// ee.style.height = "666px"
+// ee.style.marginTop = "10px"
+// ee.style.backgroundColor = "#FFA07A"
+// ee.innerHTML="vvvvvvvvv"
+// document.body.append(ee)
+
+// iv.append(ee)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let end = document.createElement("div")
+// end.style.width = "100%"
+// end.style.height = "150px"
+// end.style.display="flex"
+// iv.append(end)
+// obs.append(end)
+
+
+// let e = document.createElement("div")
+// e.style.width = "100%"
+// e.style.height = "60px"
+// e.style.marginTop = "10px"
+// e.style.marginLeft="50px"
+// e.style.display="flex"
+// let ek=document.createElement("img")
+// ek.style.backgroundImage="url(./img/)"
+// ek.style.backgroundSize="cover"
+// ek.style.width="50px"
+// ek.style.height="50px"
+// e.append(ek)
+// document.body.append(e)
+// end.append(e)
+
+// let er = document.createElement("div")
+// er.style.width = "100%"
+// er.style.height = "500px"
+// er.style.marginTop = "10px"
+// er.style.display="flex"
+// er.innerHTML="г.Краснодар,ул.Калилина 19/1"
+// document.body.append(er)
+// e.append(er)
+// end.style.backgroundColor = "#999950"
+// document.body.append(end)
+// iv.append(end)
+
+
+
+
+// let o = document.createElement("div")
+// o.style.width = "100%"
+// o.style.height = "60px"
+// o.style.marginTop = "10px"
+// o.style.display="flex"
+// let ok=document.createElement("img")
+// ok.style.backgroundImage="url(./img/)"
+// ok.style.backgroundSize="cover"
+// ok.style.width="50px"
+// ok.style.height="50px"
+// o.append(ok)
+// document.body.append(o)
+// end.append(o)
+
+// let or = document.createElement("div")
+// or.style.width = "100%"
+// or.style.height = "500px"
+// or.style.marginTop = "10px"
+// or.style.display="flex"
+// or.innerHTML="йо-х-хо_2001"
+// document.body.append(or)
+// o.append(or)
+// end.style.backgroundColor = "#999950"
+// document.body.append(end)
+// iv.append(end)
